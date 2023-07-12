e9bbc863646df1804562ee3ab06dc2b22f61c3f2
gustavogrijalba <gur73734@gmail.com>
2023-07-12 15:58:04 -0700

    created routing on the frontend

--- ear-health-project-ui/src/components/App/App.jsx
+++ ear-health-project-ui/src/components/App/App.jsx
@@ -3,6 +3,12 @@ import reactLogo from '../../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from "../Navbar/Navbar.jsx"
+import Home from "../Home/Home.jsx"
+import RegisterPage from "../RegisterPage/RegisterPage"
+import LoginPage from "../LoginPage/LoginPage"
+import Footer from "../Footer/Footer"
+import Forum from "../Forum/Forum"
+import {BrowserRouter, Routes , Route, Link} from "react-router-dom"
 
 function App() {
   
@@ -10,6 +16,17 @@ function App() {
     <>
       <div>
         <Navbar></Navbar>
+
+        <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<Home/>}/>
+          <Route path = "/register" element = {<RegisterPage/>}/>
+          <Route path = "/login" element = {<LoginPage/>}/>
+          <Route path = "/forum" element = {<Forum/>}/>
+        </Routes>
+        </BrowserRouter>
+
+        <Footer/>
         
       </div>
     </>
---
+++ ear-health-project-ui/src/components/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./RegisterPage.css"
+
+const RegisterPage = () => {
+  return (
+    <div>RegisterPage</div>
+  )
+}
+
+export default RegisterPage
