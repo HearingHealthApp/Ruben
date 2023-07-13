72232b10f9e1d408b2a8902e75d78468412152c1
JessicaFlo25 <jessica.flores309@myhunter.cuny.edu>
2023-07-13 14:48:09 -0700

    Added Login Inputs

--- ear-health-project-ui/src/components/LoginPage/LoginPage.jsx
+++ ear-health-project-ui/src/components/LoginPage/LoginPage.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
 import "./LoginPage.css"
+import { useState } from 'react'
 
 const LoginPage = () => {
+
+  const [email,setEmail]= useState("")
+  const [password,setPassword]= useState("")
+  
+  //need submit handler
+
+
+
   return (
     <div>
       <div className='login-container'>
-        <form className='login-form'>
-          <div>
-            
+        <form className='login-form' >
+          <div className='welcome'>
+            <p>Login</p> <br></br>
+          </div>
+          <div className='login-inputs'>
+          <label>Email:</label>
+          <input
+          
+          type='email'
+          value={email}
+          onChange={(e) => {setEmail(e.target.value)}}
+          required
+          placeholder='Email'
+          />
+          <label>Password</label>
+          <input
+          type="text"
+          value={password}
+          onChange={(e) => {setPassword(e.target.value)}}
+          required
+          placeholder='Password'
+          />
           </div>
         </form>
       </div>
