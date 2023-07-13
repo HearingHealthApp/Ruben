09c4596ad70024dc8a3e92133a3baa3a7c09e801
Zyanik <gerardoperez8a@gmail.com>
2023-07-13 13:28:11 -0700

    Added function that creates database Uri and  console logs new info

--- ear-health-project-api/config.js
+++ ear-health-project-api/config.js
@@ -3,16 +3,36 @@ require("colors");
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3001;
 
+const getDatabaseUri = () => {
+  const dbUser = process.env.DATABASE_USER || "postgres";
+  const dbPass = process.env.DATABASE_PASS
+    ? encodeURI(process.env.DATABASE_PASS)
+    : "postgres";
+  const dbHost = process.env.DATABASE_HOST || "localhost";
+  const dbPort = process.env.DATABASE_PORT || 5432;
+  const dbName = process.env.DATABASE_NAME || "hearinghealth";
+
+  // if DATABASE_URL environment variable, use that,
+  // otherwise create the db connection string ourself
+
+  return (
+    process.env.DATABASE_URL ||
+    `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+  );
+};
+
 const BCRYPT_WORK_FACTOR = process.env.BCRYPT_WORK_FACTOR;
 
 const SECRET_KEY = process.env.SECRET_KEY;
 
-console.log("Hearing Health App API config".red);
+console.log("Hearinghealth API config".red);
 console.log("PORT:".blue, PORT);
+console.log("Database URI:".blue, getDatabaseUri());
 console.log("-----");
 
 module.exports = {
   PORT,
+  getDatabaseUri,
   BCRYPT_WORK_FACTOR,
   SECRET_KEY,
 };
