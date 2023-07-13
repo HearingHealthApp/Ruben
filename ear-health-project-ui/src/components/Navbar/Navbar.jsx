764f7ddc79639f4a65bb50f3d0c70fc62661ede3
JessicaFlo25 <jessica.flores309@myhunter.cuny.edu>
2023-07-13 13:18:16 -0700

    updated navbar

--- ear-health-project-ui/src/components/Navbar/Navbar.jsx
+++ ear-health-project-ui/src/components/Navbar/Navbar.jsx
@@ -1,37 +1,23 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({isLoggedIn}) => {
   return (
     <div>
-      <nav className='navbar'>
-        <ul className='nav-links'>
+      <nav>
+        <ul>
           <li>
             <Link to="/">
-              <img src='https://t4.ftcdn.net/jpg/02/61/77/53/360_F_261775338_nEPbSAfDLTAo8JUguMzsxXttBPyNZZqC.jpg' alt="logo"/>
-            </Link>
-          </li>
-          <li>
-            <Link to="/aboutus">  
-            {/* need to define routes */}
-              <p>About us</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/generalinfo">
-              <p>General Information</p>
+              <img src='https://t4.ftcdn.net/jpg/02/61/77/53/360_F_261775338_nEPbSAfDLTAo8JUguMzsxXttBPyNZZqC.jpg' alt='logo'/>
             </Link>
           </li>
           <li>
             <Link to="/forum">
-              <p>
-                Forum
-              </p>
+              <p>Forum</p>
             </Link>
           </li>
-
         </ul>
-
+        {i}
       </nav>
     </div>
   )
