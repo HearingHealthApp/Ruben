63dc48bbb4a1cfd7eab0a1c77fb7b658c41c2651
Zyanik <gerardoperez8a@gmail.com>
2023-07-16 20:36:05 -0700

    Prevents is_doctor from being Null

--- ear-health-project-api/models/user.js
+++ ear-health-project-api/models/user.js
@@ -1,101 +1,230 @@
 //importing necessary dependencies
-const db = require("../db")
-const bcrypt = require("bcrypt")
-const { BCRYPT_WORK_FACTOR } = require("../config")
-const { BadRequestError, UnauthorizedError } = require("../utils/errors")
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+const { BadRequestError, UnauthorizedError } = require("../utils/errors");
 
 class User {
+  //function that will register general users based on required inputted credentials
 
-    //function that will register general users based on required inputted credentials
+  static async register(credentials) {
+    //required fields that will be taken from our register input form
+    const requiredFields = [
+      "username",
+      "email",
+      "password",
+      "firstName",
+      "lastName",
+    ];
 
-    static async register(credentials) {
+    //iterate through the required fields and check if one is missing
+    //if so, throw an error
+    requiredFields.forEach((field) => {
+      if (!credentials.hasOwnProperty(field)) {
+        throw new BadRequestError(`Missing ${field} in request body`);
+      }
+    });
 
-        //required fields that will be taken from our register input form
-        const requiredFields = ["username", "email", "password", "first_name", "last_name"]
+    //lowercase the email so that it is not case sensitive when doing checks
+    const lowercasedEmail = credentials.email.toLowerCase();
 
-        //iterate through the required fields and check if one is missing
-        //if so, throw an error
-        requiredFields.forEach(field => {
-            if(!credentials.hasOwnProperty(field)) {
-                throw new BadRequestError(`Missing ${field} in request body`)
-            }
-        })
+    if (credentials.email.indexOf("@") <= 0) {
+      throw new BadRequestError("Invalid email.");
+    }
 
-        //lowercase the email so that it is not case sensitive when doing checks
-        const lowercasedEmail = credentials.email.toLowerCase()
+    // make sure no user already exists in the system with the email
+    // if one does, throw an error
+    const existingEmail = await User.fetchUserByEmail(credentials.email);
 
-        //hash the user's password using bcrypt and salt 
-        //generate our salt using our work factor
-        const salt =  await bcrypt.genSalt(10)
+     // if user with that email is found, throw an error saying its a duplicate
+    if (existingEmail) {
+      throw new BadRequestError(`Duplicate email: ${credentials.email}`);
+    }
 
-        //use salt to hash our pasword 
-        const hashedPassword = await bcrypt.hash(credentials.password, salt)
+    // check if there's a user with the same email
+    const existingUsername = await User.fetchUserByUsername(credentials.username);
 
-        //create the user query to post data into the database
-        const userQuery = `
+    // if user with that username is found, throw an error saying its a duplicate
+    if (existingUsername) {
+      throw new BadRequestError(`Duplicate usernam: ${credentials.username}`);
+    }
+
+    //hash the user's password using bcrypt and salt
+    //generate our salt using our work factor
+    const salt = await bcrypt.genSalt(BCRYPT_WORK_FACTOR);
+
+    //use salt to hash our pasword
+    const hashedPassword = await bcrypt.hash(credentials.password, salt);
+
+    //create the user query to post data into the database
+    const userQuery = `
         INSERT INTO users (username, email, password, first_name, last_name)
         VALUES ($1, $2, $3, $4, $5)
         RETURNING *
         `;
 
-        //values we will assign 
-        const values = [credentials.username, lowercasedEmail, hashedPassword, credentials.first_name, credentials.last_name]
+    //values we will assign
+    const values = [
+      credentials.username,
+      lowercasedEmail,
+      hashedPassword,
+      credentials.firstName,
+      credentials.lastName,
+    ];
 
-        //posting to the db
-        const result = await db.query(userQuery, values)
+    //posting to the db
+    const result = await db.query(userQuery, values);
 
-        //get the user from the query 
-        const user = result.rows[0]
+    //get the user from the query
+    const user = result.rows[0];
 
-        //return the user 
-        return user
+    //return the user
+    return user;
+  }
+
+  // function to register a doctor
+  static async registerDoctor(credentials) {
+    //required fields that will be taken from our register input form
+    const requiredFields = [
+      "username",
+      "email",
+      "password",
+      "firstName",
+      "lastName",
+      "registrationNumber"
+    ];
+
+    //iterate through the required fields and check if one is missing
+    //if so, throw an error
+    requiredFields.forEach((field) => {
+      if (!credentials.hasOwnProperty(field)) {
+        throw new BadRequestError(`Missing ${field} in request body`);
+      }
+    });
+
+    //lowercase the email so that it is not case sensitive when doing checks
+    const lowercasedEmail = credentials.email.toLowerCase();
+
+    if (credentials.email.indexOf("@") <= 0) {
+      throw new BadRequestError("Invalid email.");
     }
 
-    //function that will login the user if the password and email combination match
-    static async login(credentials) {
-        //required fields are email and password
-        const requiredFields = ["email", "password"]
+    // make sure no user already exists in the system with the email
+    // if one does, throw an error
+    const existingEmail = await User.fetchUserByEmail(credentials.email);
 
-        //iterate through the required fields and check if one is missing
-        //if so, throw an error
-        requiredFields.forEach(field => {
-            if(!credentials.hasOwnProperty(field)) {
-                throw new BadRequestError(`Missing ${field} in request body`)
-            }
-        })
-
-        //check if the email entered by the user exists in the DB.
-        const user = await User.fetchUserByEmail(credentials.email)
-
-        //if user exists, check if password is valid and return the user if so
-
-        if (user) {
-            const isPasswordValid = await bcrypt.compare(credentials.password, user.password)
-            if (isPasswordValid) {
-                return user
-            }
-        }
-
-        //send error message if password isn't valid 
-        throw new UnauthorizedError("Invalid password")
-
+     // if user with that email is found, throw an error saying its a duplicate
+    if (existingEmail) {
+      throw new BadRequestError(`Duplicate email: ${credentials.email}`);
     }
 
-    //fetch an existing user based on an email
-    static async fetchUserByEmail(email) {
-        //throw an error if there is no email 
-        if (!email) {
-            throw new BadRequestError("No email provided")
-        }
-        //select from the table the user where the email matches the input
-        const query = `SELECT * FROM users WHERE email = $1`
+    // check if there's a user with the same email
+    const existingUsername = await User.fetchUserByUsername(credentials.username);
 
-        const result = await db.query(query, [email.toLowerCase()])
-
-        const user = result.rows[0]
-
-        return user 
+    // if user with that username is found, throw an error saying its a duplicate
+    if (existingUsername) {
+      throw new BadRequestError(`Duplicate usernam: ${credentials.username}`);
     }
+
+    //hash the user's password using bcrypt and salt
+    //generate our salt using our work factor
+    const salt = await bcrypt.genSalt(BCRYPT_WORK_FACTOR);
+
+    //use salt to hash our pasword
+    const hashedPassword = await bcrypt.hash(credentials.password, salt);
+
+    //create the user query to post data into the database
+    const userQuery = `
+        INSERT INTO users (username, email, password, first_name, last_name)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING *
+        `;
+
+    //values we will assign
+    const values = [
+      credentials.username,
+      lowercasedEmail,
+      hashedPassword,
+      credentials.first_name,
+      credentials.last_name,
+    ];
+
+    //posting to the db
+    const result = await db.query(userQuery, values);
+
+    //get the user from the query
+    const user = result.rows[0];
+
+    //return the user
+    return user;
+  }
+
+  //function that will login the user if the password and email combination match
+  static async login(credentials) {
+    //required fields are email and password
+    const requiredFields = ["email", "password"];
+
+    //iterate through the required fields and check if one is missing
+    //if so, throw an error
+    requiredFields.forEach((field) => {
+      if (!credentials.hasOwnProperty(field)) {
+        throw new BadRequestError(`Missing ${field} in request body`);
+      }
+    });
+
+    //check if the email entered by the user exists in the DB.
+    const user = await User.fetchUserByEmail(credentials.email);
+
+    //if user exists, check if password is valid and return the user if so
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(
+        credentials.password,
+        user.password
+      );
+      if (isPasswordValid) {
+        return user;
+      }
+    }
+
+    //send error message if password isn't valid
+    throw new UnauthorizedError("Invalid password");
+  }
+
+  //fetch an existing user based on an email
+  static async fetchUserByEmail(email) {
+    //throw an error if there is no email
+    if (!email) {
+      throw new BadRequestError("No email provided");
+    }
+    //select from the table the user where the email matches the input
+    const query = `SELECT * FROM users WHERE email = $1`;
+
+    const result = await db.query(query, [email.toLowerCase()]);
+
+    const user = result.rows[0];
+
+    return user;
+  }
+
+  // fetch an existing user based on username
+  static async fetchUserByUsername(username) {
+    // throw an error if no username is provided
+    if (!username) {
+      throw new BadRequestError("No username provided");
+    }
+
+    // select from the users table the the user that matches the username
+    const query = `SELECT * FROM users WHERE username = $1`;
+
+    // convert the provided username to all lowercase and use it as the reference
+    // username in the query
+    const result = await db.query(query, [username.toLowerCase()]);
+
+    const user = result.rows[0];
+
+    return user;
+  }
 }
 
-module.exports = User
+module.exports = User;
--- ear-health-project-api/routes/authRoutes.js
+++ ear-health-project-api/routes/authRoutes.js
@@ -1,42 +1,59 @@
-//installing necessary dependencies to set up the routes
-const express = require("express")
+// installing necessary dependencies to set up the routes
+const express = require("express");
 
-//model import
-const User = require("../models/user")
-const {createUserJwt} = require("../utils/tokens")
-const { SECRET_KEY } = require("../config")
+// model import
+const User = require("../models/user");
+const { createUserJwt } = require("../utils/tokens");
+const { SECRET_KEY } = require("../config");
 
-//setting up the router
-const router = express.Router()
+// setting up the router
+const router = express.Router();
 
-//post route for registering 
+// post route for registering
 router.post("/register", async (req, res, next) => {
-    try {
-    //register the user
-    const user = await User.register(req.body)
-    //create the token 
-    const token = createUserJwt(user)
-    //return the user and the token as a json object
-    return res.status(201).json({user: user, token: token})
+  try {
+    // register the user
+    const user = await User.register(req.body);
 
-    } catch(err) {
-        next(err)
-    }
+    // create the token
+    const token = createUserJwt(user);
 
-})
+    // return the user and the token as a json object
+    return res.status(201).json({ user: user, token: token });
+  } catch (err) {
+    next(err);
+  }
+});
 
 //post route for login
-router.post("/login", async (req, res,next) => {
-    try {
-        const user = await User.login(req.body)
+router.post("/login", async (req, res, next) => {
+  try {
+    // log in the user
+    const user = await User.login(req.body);
 
-        const token = createUserJwt(user)
+    // create the token
+    const token = createUserJwt(user);
 
-        return res.status(200).json({user: user, token: token})
+    // return the user and the token as a json object
+    return res.status(200).json({ user: user, token: token });
+  } catch (err) {
+    next(err);
+  }
+});
 
-        } catch(err) {
-            next(err)
-        }
-})
+router.post("/register/doctor", async (req, res, next) => {
+  try {
+    // register the doctor
+    const user = await User.registerDoctor(req.body);
 
-module.exports = router
+    // create the token
+    const token = createUserJwt(user);
+
+    // return the user and the token as a json object
+    return res.status(201).json({ user: user, token: token });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
