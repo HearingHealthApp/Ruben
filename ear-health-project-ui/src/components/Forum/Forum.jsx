c367deb79d3f5939863961bc80a2a1a3c484e98e
gustavogrijalba <gur73734@gmail.com>
2023-07-17 10:20:37 -0700

    forum frontend

--- ear-health-project-ui/src/components/Forum/Forum.jsx
+++ ear-health-project-ui/src/components/Forum/Forum.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Forum.css"
+import ForumPrompt from '../ForumPrompt/ForumPrompt'
 
 const Forum = () => {
+
+  const [createPostTrue, setCreatePostTrue] = useState(false)
+
+  const clickButton = () => {
+    setCreatePostTrue(true)
+  }
+  const cancelButton = () => {
+    setCreatePostTrue(false)
+  }
+
   return (
-    <div>Forum</div>
+    <div>
+      <input type = "text" placeholder = 'Search for a post'/>
+      <br/>
+
+      {createPostTrue ? 
+      <div>
+      <ForumPrompt/> 
+      <button onClick={cancelButton}>Cancel</button> 
+      </div>: <button onClick={clickButton}>Create a Post</button>
+      }
+    </div>
   )
 }
 
