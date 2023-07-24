768cc7f469dae808360bccb584503768676f3691
Zyanik <gerardoperez8a@gmail.com>
2023-07-24 16:07:53 -0700

    Added notification routes to app.js

--- ear-health-project-api/app.js
+++ ear-health-project-api/app.js
@@ -16,12 +16,14 @@ const authRoutes = require("./routes/authRoutes");
 const forumRoutes = require("./routes/forumRoutes");
 const commentsRoutes = require("./routes/commentsRoutes")
 const profileRoutes = require("./routes/profileRoutes")
+const notificationRoutes = require("./routes/notificationRoutes")
 
 //use the routes
 app.use("/auth", authRoutes);
 app.use("/forum", forumRoutes);
 app.use("/comments", commentsRoutes)
 app.use("/profile", profileRoutes)
+app.use("/notifications", notificationRoutes)
 
 //error handling next functions
 app.use((req, res, next) => {
