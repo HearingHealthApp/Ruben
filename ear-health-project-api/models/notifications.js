b4026ba58bbe42c82b90c56600a8d7c204507a46
Zyanik <gerardoperez8a@gmail.com>
2023-07-24 11:43:19 -0700

    Created template for notifications model

--- ear-health-project-api/models/notifications.js
+++ ear-health-project-api/models/notifications.js
@@ -4,8 +4,18 @@ const { BadRequestError, UnauthorizedError } = require("../utils/errors");
 const convertSnakeToCamel = require("../utils/formatters");
 
 class Notification {
-    // create function that adds notification to notification table
-    static async  createNotification () {
-        // check that all fields are there 
-    }
+  // create function that adds notification to notification table
+  static async createNotification(notificationData) {
+    // check that all fields are there
+  }
+
+  static async notificationUpdater(notificationId) {
+
+  }
+
+  static async getNotificationsById(userId) {
+
+  }
+
+
 }
