37ceea12c1cb1376a6e73c195e69c30559a3a6e6
Zyanik <gerardoperez8a@gmail.com>
2023-07-26 16:34:34 -0700

    Update NotificationCard.jsx

--- ear-health-project-ui/src/components/NotificationCard/NotificationCard.jsx
+++ ear-health-project-ui/src/components/NotificationCard/NotificationCard.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import "./NotificationCard.css"
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./NotificationCard.css";
+import { useNavigate } from "react-router-dom";
+import ApiClient from "../../services/apiClient";
 
-function NotificationCard({notificationData}) {
-    console.log(notificationData)
+
+function NotificationCard({ notificationData }) {
+  console.log(notificationData);
+  let navigate = useNavigate();
+
+  const notificationNavigator = () => {
+    navigate(`/forum/post/${notificationData.postId}`)
+    ApiClient.notificationUpdater(notificationData.notificationId)
+  
+  };
   return (
-    //<Link to = {`/${}`}>
-      <div>{notificationData.message}</div>
-      //</Link>
-  )
+    <div className="notification-card">
+      <p onClick={notificationNavigator}>{notificationData.message}</p>
+    </div>
+  );
 }
 
-export default NotificationCard
+export default NotificationCard;
