7a91be4234b65a1e56def2d11e5f3c0bae187a5e
Zyanik <gerardoperez8a@gmail.com>
2023-08-07 10:35:45 -0700

    Update app.js

--- ear-health-project-api/app.js
+++ ear-health-project-api/app.js
@@ -14,20 +14,28 @@ app.use(security.extractUserFromJwt);
 //import the routes
 const authRoutes = require("./routes/authRoutes");
 const forumRoutes = require("./routes/forumRoutes");
-const commentsRoutes = require("./routes/commentsRoutes")
-const profileRoutes = require("./routes/profileRoutes")
-const notificationRoutes = require("./routes/notificationRoutes")
-const s3Routes = require ("./routes/s3Routes")
-const aiRoutes = require("./routes/aiRoutes")
+const commentsRoutes = require("./routes/commentsRoutes");
+const profileRoutes = require("./routes/profileRoutes");
+const notificationRoutes = require("./routes/notificationRoutes");
+const s3Routes = require("./routes/s3Routes");
+const aiRoutes = require("./routes/aiRoutes");
 
 //use the routes
 app.use("/auth", authRoutes);
 app.use("/forum", forumRoutes);
-app.use("/comments", commentsRoutes)
-app.use("/profile", profileRoutes)
-app.use("/notification", notificationRoutes)
-app.use("/s3", s3Routes)
-app.use("/ai", aiRoutes)
+app.use("/comments", commentsRoutes);
+app.use("/profile", profileRoutes);
+app.use("/notification", notificationRoutes);
+app.use("/s3", s3Routes);
+app.use("/ai", aiRoutes);
+
+app.get("/", async (req, res, next) => {
+  try {
+    return res.status(201).json({ message: "this is working" });
+  } catch (err) {
+    next(err);
+  }
+});
 
 //error handling next functions
 app.use((req, res, next) => {
