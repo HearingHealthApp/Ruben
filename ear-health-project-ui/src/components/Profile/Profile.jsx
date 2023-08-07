49d8c524ee29fd55bfde9b51aa06b2e7aaef906b
Zyanik <gerardoperez8a@gmail.com>
2023-08-06 22:09:51 -0700

    Revert "Merge pull request #107 from HearingHealthApp/gustavo"
    This reverts commit 7f92e262a7ea4bc455e73732a40cb79e94d21ec1, reversing
    changes made to 0ee59b28cd84c89a4e5df109a7a8478f61743a0a.

--- ear-health-project-ui/src/components/Profile/Profile.jsx
+++ ear-health-project-ui/src/components/Profile/Profile.jsx
@@ -252,19 +252,7 @@ const Profile = ({ user, setProfileImageKey }) => {
           <div className="user-interaction-container">
             <div className="user-interactions">
               {conditionalRender === "Posts"
-                ? 
-                <div>{ user.userId == userId ? 
-                  userPosts
-                    .map((post) => (
-                      <div>
-                        <Link
-                          className="link"
-                          to={`/forum/post/${post.postId}`}
-                        >
-                          <ForumPostCard key={post.postId} post={post} />
-                        </Link>
-                      </div>
-                    )): userPosts
+                ? userPosts
                     .filter((post) => !post.isAnonymous)
                     .map((post) => (
                       <div>
@@ -276,33 +264,19 @@ const Profile = ({ user, setProfileImageKey }) => {
                         </Link>
                       </div>
                     ))
-                  } </div>
                 : conditionalRender === "Comments" &&
-                <div>{user.userId == userId ? 
                   userComments
-                  .map((comment) => (
-                    <div>
-                      <Link
-                        className="link"
-                        to={`/forum/post/${comment.postId}`}
-                      >
-                        <CommentCard comment={comment} />
-                      </Link>
-                    </div>
-                  )): userComments
-                  .filter((comment) => !comment.isAnonymous)
-                  .map((comment) => (
-                    <div>
-                      <Link
-                        className="link"
-                        to={`/forum/post/${comment.postId}`}
-                      >
-                        <CommentCard comment={comment} />
-                      </Link>
-                    </div>
-                  ))
-                }</div>
-                  }
+                    .filter((comment) => !comment.isAnonymous)
+                    .map((comment) => (
+                      <div>
+                        <Link
+                          className="link"
+                          to={`/forum/post/${comment.postId}`}
+                        >
+                          <CommentCard comment={comment} />
+                        </Link>
+                      </div>
+                    ))}
             </div>
           </div>
         </div>
